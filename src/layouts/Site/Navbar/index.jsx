09f3e48cef064cd5339f8be59f86527a59c6d2f5
ahmadkybora <ahmadkybora@gmail.com>
2022-12-21 21:48:00 +0330

    add btn

--- src/layouts/Site/Navbar/index.jsx
+++ src/layouts/Site/Navbar/index.jsx
@@ -24,57 +24,80 @@ import {
 } from "../../../components";
 import { NavLink } from "react-router-dom";
 
+let articles = [
+    {
+        name: 'Article_1',
+        path: "/Article_1", 
+    },
+    {
+        name: 'Article_2',
+        path: "/Article_2", 
+    },
+    {
+        name: 'Article_3',
+        path: "/Article_3", 
+    },
+    {
+        name: 'Article_4',
+        path: "/Article_4", 
+    },
+    {
+        name: 'Article_5',
+        path: "/Article_5", 
+    },
+    {
+        name: 'Article_6',
+        path: "/Article_6", 
+    },
+    {
+        name: 'Article_7',
+        path: "/Article_7", 
+    },
+];
+
+let products = [
+    {
+        name: 'Product_1',
+        path: "/product_1", 
+    },
+    {
+        name: 'Product_2',
+        path: "/product_2", 
+    },
+    {
+        name: 'Product_3',
+        path: "/product_3", 
+    },
+    {
+        name: 'Product_4',
+        path: "/product_1", 
+    },
+    {
+        name: 'Product_5',
+        path: "/product_1", 
+    },
+    {
+        name: 'Product_6',
+        path: "/product_6", 
+    },
+    {
+        name: 'Product_7',
+        path: "/product_7", 
+    },
+]
+
 let credits = [
     {
         id: 1,
         name: "Articles", 
         icon: <GroupIcon color="success" />, 
         border: '1px dashed grey',
-        f: [
-            {name: 'Article_1',},
-            {name: 'Article_2',},
-            {name: 'Article_3',},
-            {name: 'Article_4',},
-            {name: 'Article_5',},
-            {name: 'Article_6',},
-            {name: 'Article_7',},
-        ]
     },
     {
         id: 2,
         name: "Products", 
         icon: <GroupIcon color="disabled" />, 
         border: '1px dashed grey',
-        f: [
-            {
-                name: 'Product_1',
-                path: "/product_1", 
-            },
-            {
-                name: 'Product_2',
-                path: "/product_2", 
-            },
-            {
-                name: 'Product_3',
-                path: "/product_3", 
-            },
-            {
-                name: 'Product_4',
-                path: "/product_1", 
-            },
-            {
-                name: 'Product_5',
-                path: "/product_1", 
-            },
-            {
-                name: 'Product_6',
-                path: "/product_6", 
-            },
-            {
-                name: 'Product_7',
-                path: "/product_7", 
-            },
-        ]
     },
     {
         id: 3,
@@ -91,15 +114,25 @@ let credits = [
 ];
 
 const Navbar = () => {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
+    const [anchorElArticle, setAnchorElArticle] = React.useState(null);
+    const [anchorElProduct, setAnchorElProduct] = React.useState(null);
+    const openArticle = Boolean(anchorElArticle);
+    const openProduct = Boolean(anchorElProduct);
+    const handleClickProduct = (event) => {
+        setAnchorElProduct(event.currentTarget);
+    };
+    const handleCloseProduct = () => {
+        setAnchorElProduct(null);
+    };
+    const handleClickArticle = (event) => {
+        setAnchorElArticle(event.currentTarget);
+    };
+    const handleCloseArticle = () => {
+        setAnchorElArticle(null);
     };
     const handleClose = () => {
         setAnchorEl(null);
     };
-
 // const l = () => {
 //     for(let i=0; i <= credits.length; i++) {
 //         credits.map((credit, index) => (
@@ -153,61 +186,113 @@ const Navbar = () => {
     return(
         <Widget>
             <Card sx={{ display: 'flex' }}>
-                {credits.map((credit, index) => (
-                    <Card sx={{ display: 'flex' }}>
-                        <Box 
-                            key={index} 
-                            sx={{ 
-                                my: 1, 
-                                mx: 3, 
-                                display: 'flex', 
-                                flexDirection: 'column', 
-                                backgroundColor: 'text.disabled', 
-                                border: credit.border }}
+                <Card sx={{ display: 'flex' }}>
+                    <Box 
+                        sx={{ 
+                            my: 1, 
+                            mx: 3, 
+                            display: 'flex', 
+                            flexDirection: 'column', 
+                            backgroundColor: 'text.disabled', 
+                            border:'1px dashed grey'
+                        }}
+                        >
+                        <CardContent sx={{ flex: '1 0 auto' }}>
+                            <Button
+                                id="fade-button"
+                                aria-controls={openProduct ? 'fade-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={openProduct ? 'true' : undefined}
+                                onClick={handleClickProduct}
+                            >
+                            </Button>
+                            Products
+                            <Menu
+                                id="fade-menu"
+                                MenuListProps={{
+                                'aria-labelledby': 'fade-button',
+                                }}
+                                anchorEl={anchorElProduct}
+                                open={openProduct}
+                                onClose={handleCloseProduct}
+                                TransitionComponent={Fade}
                                 >
-                            <CardContent sx={{ flex: '1 0 auto' }}>
-                                <Button
-                                    id="fade-button"
-                                    aria-controls={open ? 'fade-menu' : undefined}
-                                    aria-haspopup="true"
-                                    aria-expanded={open ? 'true' : undefined}
-                                    onClick={handleClick}
-                                >
-                                    {credit.name}
-                                </Button>
-                                <Menu
-                                    id="fade-menu"
-                                    MenuListProps={{
-                                    'aria-labelledby': 'fade-button',
-                                    }}
-                                    anchorEl={anchorEl}
-                                    open={open}
-                                    onClose={handleClose}
-                                    TransitionComponent={Fade}
-                                >
-                                    <Paper sx={{ 
-                                        width: 320, 
-                                        maxWidth: '100%', 
-                                        display: 'flex', 
-                                        flexWrap: 'wrap',
-                                        justifyContent: 'space-around',
-                                        }}>
-                                        {credits[0].f.map((n, index) => (
-                                            <MenuItem key={index} onClick={handleClose}>
-                                                <NavLink 
-                                                    style={{ textDecoration: 'none' }}
-                                                    to={n.path}
-                                                    >
-                                                        {n.name}
-                                                </NavLink>
-                                            </MenuItem>
-                                        ))}
-                                    </Paper>
-                                </Menu>
-                            </CardContent>
-                        </Box>
-                    </Card>
-                ))}
+                                <Paper sx={{ 
+                                    width: 320, 
+                                    maxWidth: '100%', 
+                                    display: 'flex', 
+                                    flexWrap: 'wrap',
+                                    justifyContent: 'space-around',
+                                    }}>
+                                    {products.map((product, index) => (
+                                        <MenuItem key={index} onClick={handleClose}>
+                                            <NavLink 
+                                                style={{ textDecoration: 'none' }}
+                                                to={product.path}
+                                                >
+                                                    {product.name}
+                                            </NavLink>
+                                        </MenuItem>
+                                    ))}
+                                </Paper>
+                            </Menu>
+                        </CardContent>
+                    </Box>
+                </Card>
+
+                <Card sx={{ display: 'flex' }}>
+                    <Box 
+                        sx={{ 
+                            my: 1, 
+                            mx: 3, 
+                            display: 'flex', 
+                            flexDirection: 'column', 
+                            backgroundColor: 'text.disabled', 
+                            border: '1px dashed grey'
+                        }}
+                        >
+                        <CardContent sx={{ flex: '1 0 auto' }}>
+                            <Button
+                                id="fade-button"
+                                aria-controls={openArticle ? 'fade-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={openArticle ? 'true' : undefined}
+                                onClick={handleClickArticle}
+                            >
+                            </Button>
+                            Articles
+                            <Menu
+                                id="fade-menu"
+                                MenuListProps={{
+                                'aria-labelledby': 'fade-button',
+                                }}
+                                anchorEl={anchorElArticle}
+                                open={openArticle}
+                                onClose={handleCloseArticle}
+                                TransitionComponent={Fade}
+                            >
+                                <Paper sx={{ 
+                                    width: 320, 
+                                    maxWidth: '100%', 
+                                    display: 'flex', 
+                                    flexWrap: 'wrap',
+                                    justifyContent: 'space-around',
+                                    }}>
+                                    {articles.map((article, index) => (
+                                        <MenuItem key={index} onClick={handleClose}>
+                                            <NavLink 
+                                                style={{ textDecoration: 'none' }}
+                                                to={article.path}
+                                                >
+                                                    {article.name}
+                                            </NavLink>
+                                        </MenuItem>
+                                    ))}
+                                </Paper>
+                            </Menu>
+                        </CardContent>
+                    </Box>
+                </Card>
             </Card>
         </Widget>
     )
